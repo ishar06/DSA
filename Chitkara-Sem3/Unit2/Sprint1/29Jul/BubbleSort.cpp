ee1f4df46eaed504b1549cd787fb53a53a605acc
Ishardeep Singh <singhishardeep06@gmail.com>
2025-08-19 00:24:15 +0530

    Update BubbleSort.cpp

--- Chitkara-Sem3/Unit2/Sprint1/29Jul/BubbleSort.cpp
+++ Chitkara-Sem3/Unit2/Sprint1/29Jul/BubbleSort.cpp
@@ -3,6 +3,19 @@
 #include <vector>
 using namespace std;
 
+void bubble_sort(int arr[], int n) {
+  int i, j, temp;
+  for (i = 0; i < n - 1; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
 void bubbleSort(vector<int>arr, int n){
     for (int i=0;i<n-1;i++){
         bool flag = false;
