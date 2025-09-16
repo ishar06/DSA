5eb3f90aa8455f1eafa78a8223e15ae7f0b78748
Ishardeep Singh <singhishardeep06@gmail.com>
2025-09-16 17:37:38 +0530

    LL Stack

---
+++ Chitkara-Sem3/ST2/16Sep/LLStacks.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = nullptr;
+    }
+};
+
+class LLStack{
+    private:
+    Node* top;
+    int size;
+
+    public:
+    LLStack(){
+        top = NULL;
+        size = 0;
+    }
+
+    void push(int val){
+        Node* newNode = new Node(val);
+        newNode->next = top;
+        top = newNode;
+        cout << val << " pushed to stack." << endl;
+        size++;
+    }
+
+    int pop(){
+        if (top->next == NULL){
+            cout << "Stack is empty." << endl;
+            return -1;
+        }
+        int item = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        cout << item << " popped from the stack." << endl;
+        size--;
+        return item;
+    }
+
+    int peek(){
+        if(top == NULL){
+            cout << "Stack is empty." << endl;
+            return -1;
+        }
+        int item = top->data;
+        return item;
+    }
+
+    bool isEmpty(){
+        return (top==NULL);
+    }
+
+    int getSize(){
+        return size;
+    }
+
+    void display(){
+        if(top == NULL){
+            cout << "Stack is empty." << endl;
+            return;
+        }
+        Node* temp = top;
+        while(temp != nullptr){
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+
+};
+
+int main(){
+
+    LLStack stack;
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+    stack.display();
+
+}
